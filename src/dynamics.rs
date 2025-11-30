27b0779cb4eec6c986425af19cc0d21d4f0da924
Jan Hohenheim <jan@hohenheim.ch>
2025-11-30 12:22:31 +0100

    Use char mass and rel vel

--- src/dynamics.rs
+++ src/dynamics.rs
@@ -12,29 +12,30 @@ pub(super) fn plugin(schedule: Interned<dyn ScheduleLabel>) -> impl Fn(&mut App)
 }
 
 fn apply_forces(
-    kccs: Query<&CharacterControllerState>,
+    kccs: Query<(&ComputedMass, &CharacterControllerState)>,
     colliders: Query<&ColliderOf>,
     mut rigid_bodies: Query<(&RigidBody, Forces)>,
 ) {
-    for touch in kccs.iter().flat_map(|state| state.touching_entities.iter()) {
-        let Ok(collider_of) = colliders.get(touch.entity) else {
-            continue;
-        };
-        let Ok((rigid_body, mut forces)) = rigid_bodies.get_mut(collider_of.body) else {
-            continue;
-        };
-        if !rigid_body.is_dynamic() {
-            continue;
-        }
-        // TODO: not on step up
+    for (mass, state) in &kccs {
+        let mass = mass.value();
+        for touch in &state.touching_entities {
+            let Ok(collider_of) = colliders.get(touch.entity) else {
+                continue;
+            };
+            let Ok((rigid_body, mut forces)) = rigid_bodies.get_mut(collider_of.body) else {
+                continue;
+            };
+            if !rigid_body.is_dynamic() {
+                continue;
+            }
+            // TODO: not on step up
 
-        let touch_dir = -touch.normal;
-        // TODO: read from character
-        let mass = 80.0;
-        // TODO: use relative vel
-        let velocity = touch.character_velocity;
-        let touch_velocity = touch_dir.dot(velocity) * touch_dir;
-        let impulse = touch_velocity * mass;
-        forces.apply_linear_impulse_at_point(impulse, touch.point);
+            let touch_dir = -touch.normal;
+            let relative_velocity = touch.character_velocity - forces.linear_velocity();
+            let touch_velocity = touch_dir.dot(relative_velocity) * touch_dir;
+            let impulse = touch_velocity * mass;
+
+            forces.apply_linear_impulse_at_point(impulse, touch.point);
+        }
     }
 }
